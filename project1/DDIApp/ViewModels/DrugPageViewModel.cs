0dd08988bac55c625f7aecd97354f9e42a158631
Martin Fetzel <U01K1RM@post.at>
2020-06-14 19:48:33 +0200

    #6 2.1.3.2 remove drug, view drug information 2.1.2

--- project1/DDIApp/ViewModels/DrugPageViewModel.cs
+++ project1/DDIApp/ViewModels/DrugPageViewModel.cs
@@ -1,20 +1,27 @@
 ﻿using DDILibrary;
+using Prism.Commands;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace DDIApp.ViewModels
 {
     public class DrugPageViewModel : ViewModelBase
     {
-        public DrugPageViewModel(INavigationService navigationService) : base(navigationService)
+        private readonly DrugService _drugService;
+        public DrugPageViewModel(
+            INavigationService navigationService,
+            DrugService drugService)
+            : base(navigationService)
         {
-            
+            _drugService = drugService;
+            RemoveCommand = new DelegateCommand<Drug>(OnRemoveCommand);
         }
         public override void Initialize(INavigationParameters parameters)
         {
-            if(parameters.ContainsKey(nameof(Drug)))
+            if (parameters.ContainsKey(nameof(Drug)))
             {
                 Drug = parameters[nameof(Drug)] as Drug;
             }
@@ -25,6 +32,21 @@ namespace DDIApp.ViewModels
         {
             get { return _Drug; }
             set { SetProperty(ref _Drug, value, nameof(Drug)); }
+        }
+
+        public ICommand RemoveCommand { get; }
+
+        protected void OnRemoveCommand(Drug drug)
+        {
+            if (drug != null)
+            {
+                _drugService.Drugs.Remove(drug);
+                _drugService.SaveDrugs();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                NavigationService.NavigateAsync(nameof(MainPageViewModel));
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            }
         }
     }
 }
